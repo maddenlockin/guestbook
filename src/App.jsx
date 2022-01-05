57ff8e57f15e1dad36b3583d888282c09985a42b
maddenlockin <maddenlockin@gmail.com>
2022-01-04 16:03:25 -0800

    PrivateRoute created and applied to App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { UserProvider } from "./context/UserContext";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from "./views/Home/Home";
 import Layout from "./views/Layout/Layout";
 import React from "react";
@@ -7,9 +8,20 @@ import { ProvideAuth } from "./context/ProvideAuth";
 export default function App() {
   return (
     <ProvideAuth>
-      <Layout>
-        <Home />
-      </Layout>
+      <Router>
+        <Route path='/login'>
+          <Login />
+        </Route>
+        <PrivateRoute path='/guestbook'>
+          <Layout>
+            <Home />
+          </Layout>
+        </PrivateRoute>
+      </Router>
     </ProvideAuth>
   );
 }
+
+{/* <Layout>
+  <Home />
+</Layout>; */}
